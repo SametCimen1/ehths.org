66de172844cdffd6d234c6625cb92ba54b712128
SametCimen1 <cimensamet338@gmail.com>
2023-02-28 20:02:41 -0500

    chaned bcrypt again

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 dotenv.config();
 
 module.exports = async (req,res, next) => {
