8e6e64be2844298913f33b21768e8c1d59fa72b6
SametCimen1 <cimensamet338@gmail.com>
2023-02-28 19:54:06 -0500

    bcrypt

--- server.js
+++ server.js
@@ -345,7 +345,11 @@ io.on('connection',(socket) => {
       }
    })
 
-   app.get("*", (req, res) => {
+   app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+    });
+    
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client/build/index.html"))
    })
 });
